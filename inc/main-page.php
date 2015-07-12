dc09e7c6df9eefabbcf75af90a19738fe3b5ee4e
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-12 06:26:50 +0000

    AccessPress Twitter Auto Post - Added a note for PHP version required
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-twitter-auto-post/tags/1.0.3@1197303 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/main-page.php
@@ -0,0 +1,51 @@
+<div class="wrap">
+    <!--Plugin Header-->
+    <?php include('header.php'); ?>
+    <!--Plugin Header-->
+
+    <div class="asap-main-section">
+        <?php if (isset($_SESSION['atap_message'])) { ?><p class="asap-message"><?php
+            echo $_SESSION['atap_message'];
+            unset($_SESSION['atap_message']);
+            ?></p><?php } ?>
+        <?php
+        global $wpdb;
+        //include('networks/facebook.php');
+        
+        
+            ?>
+            <?php $active_tab = isset($_GET['tab']) ? $_GET['tab'] : 'settings'; ?>
+            <div class="asap-main-inner-wrap">
+                <ul class="asap-tabs-wrap">
+                    <li class="asap-tab <?php if ($active_tab == 'settings') { ?>asap-active-tab<?php } ?>" id="asap-tab-settings"><?php _e('Settings', ATAP_TD); ?></li>
+                    <li class="asap-tab <?php if ($active_tab == 'logs') { ?>asap-active-tab<?php } ?>" id="asap-tab-logs"><?php _e('Logs', ATAP_TD); ?></li>
+                    <li class="asap-tab <?php if ($active_tab == 'how') { ?>asap-active-tab<?php } ?>" id="asap-tab-how"><?php _e('How To Use', ATAP_TD); ?></li>
+                    <li class="asap-tab <?php if ($active_tab == 'about') { ?>asap-active-tab<?php } ?>" id="asap-tab-about"><?php _e('About', ATAP_TD); ?></li>
+                </ul>
+                <?php
+                /**
+                 * Accounts Section
+                 */
+                include_once('tabs/settings.php');
+
+                /**
+                 * Logs Section
+                 * */
+                include('tabs/logs.php');
+
+                /**
+                 * How To Use Section
+                 */
+                include_once('tabs/how-to-use.php');
+
+                /**
+                 * About Section
+                 */
+                include_once('tabs/about.php');
+                ?>
+
+
+            </div>
+           
+    </div>
+</div>
